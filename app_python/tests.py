4b643ac2d72524b30710e845e705363a31b7557a
Lev Chelyadinov <leva181777@gmail.com>
2021-08-25 13:47:27 +0300

    Fix the tests

--- app_python/tests.py
+++ app_python/tests.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 import pytest
 from sanic.websocket import WebSocketProtocol
 
 from main import app
+
+moscow_tz = timezone(timedelta(hours=3))
 
 
 @pytest.fixture
@@ -12,7 +14,7 @@ def test_app(loop, sanic_client):
 
 
 async def test_time_correctness(test_app):
-    current_time = datetime.now()
+    current_time = datetime.now(tz=moscow_tz)
     response = await test_app.get('/')
     assert response.status_code == 200
     assert current_time.strftime('%H:%M:%S') == response.text
