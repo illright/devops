981212bced2d41621128251a9c6e5a6bc48c08c0
Lev Chelyadinov <leva181777@gmail.com>
2021-09-26 20:31:36 +0300

    Fix linter errors

--- app_python/main.py
+++ app_python/main.py
@@ -9,16 +9,17 @@ from sanic.response import text
 app = Sanic('moscow-time-app')
 moscow_tz = timezone(timedelta(hours=3))
 
-visits_filename = 'visits.txt'
+VISITS_FILENAME = 'visits.txt'
 
 @app.get("/")
 async def report_time(_request):
     '''Returns the time in Moscow'''
 
-    if not os.path.exists(visits_filename):
-        open(visits_filename, 'w').close()
+    if not os.path.exists(VISITS_FILENAME):
+        with open(VISITS_FILENAME, 'w', encoding='utf-8'):
+            pass
 
-    with open(visits_filename, 'r+') as visits_file:
+    with open(VISITS_FILENAME, 'r+', encoding='utf-8') as visits_file:
         last_visits = int(visits_file.read() or 0)
         visits_file.seek(0)
         visits_file.write(str(last_visits + 1))
@@ -26,11 +27,11 @@ async def report_time(_request):
     return text(datetime.now(tz=moscow_tz).strftime('%H:%M:%S'))
 
 @app.get("/visits")
-async def report_time(_request):
+async def report_visits(_request):
     '''Returns the amount of visits to the root of the website.'''
 
-    if os.path.exists(visits_filename):
-        with open(visits_filename, 'r') as visits_file:
+    if os.path.exists(VISITS_FILENAME):
+        with open(VISITS_FILENAME, 'r', encoding='utf-8') as visits_file:
             return text(visits_file.read())
     else:
         return text('0')
